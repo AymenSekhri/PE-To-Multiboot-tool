b25bd9dd2deb7af03ad287c1c6dedbd9918f3b1d
ASekhri <aymen.sekhri@live.fr>
2019-10-08 15:50:11 +0100

    path edits

--- Multiboot.cpp
+++ Multiboot.cpp
@@ -1,8 +1,9 @@
 // Multiboot.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <string>
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 struct MULTIBOOT_HEADER {
 	UINT32 magic;
 	UINT32 flags;
@@ -13,9 +14,18 @@ struct MULTIBOOT_HEADER {
 	UINT32 bss_end_addr;
 	UINT32 entry_addr;
 };
+std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
+	size_t start_pos = 0;
+	while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+	}
+	return str;
+}
 #define LOADBASE 0x00100000 // default load base of grub
 int main(int argc, char** argv)
 {	
+
 	//Open PE file.
 	printf("Adding MultiBoot header to : %s.\n", argv[1]);
 	HANDLE hFile = CreateFileA(argv[1], GENERIC_WRITE | GENERIC_READ, NULL, NULL, OPEN_EXISTING, NULL, NULL);
@@ -29,7 +39,7 @@ int main(int argc, char** argv)
 	char* file = (char*)VirtualAlloc(0, filesize, MEM_COMMIT, PAGE_READWRITE);
 	if (ReadFile(hFile, file, filesize, &retsize, 0) == 0)
 	{
-		printf("Could not read the file Error :%X.", GetLastError());
+		printf("Could not read the file Error :%X.\n", GetLastError());
 		VirtualFree(file, 0, MEM_RELEASE);
 		CloseHandle(hFile);
 		return TRUE;
@@ -38,7 +48,7 @@ int main(int argc, char** argv)
 	PIMAGE_NT_HEADERS NT = (PIMAGE_NT_HEADERS)(file + DOS->e_lfanew);
 	if (NT->Signature != IMAGE_NT_SIGNATURE)
 	{
-		printf("Not executable.");
+		printf("Not valid executable.\n");
 		VirtualFree(file, 0, MEM_RELEASE);
 		CloseHandle(hFile);
 		return TRUE;
@@ -46,7 +56,7 @@ int main(int argc, char** argv)
 	PIMAGE_SECTION_HEADER ISH = (PIMAGE_SECTION_HEADER)((char*)NT + sizeof(IMAGE_NT_HEADERS));
 	if (((int)ISH - int(file) + NT->FileHeader.NumberOfSections * sizeof(IMAGE_SECTION_HEADER) + 0x20) > ISH[0].PointerToRawData)
 	{
-		printf("There is no enough space for multiboot header.\nNote:File Alignment should be 0x1000");
+		printf("There is no enough space for multiboot header.\nNote:File Alignment should be 0x1000.\n");
 		VirtualFree(file, 0, MEM_RELEASE);
 		CloseHandle(hFile);
 		return TRUE;
@@ -68,14 +78,22 @@ int main(int argc, char** argv)
 	SetFilePointer(hFile, 0, 0, 0);
 	if (WriteFile(hFile, file, filesize, &retsize, 0) == 0)
 	{
-		printf("Could not write to the file Error :%X.", GetLastError());
+		printf("Could not write to the file Error :%X.\n", GetLastError());
 		VirtualFree(file, 0, MEM_RELEASE);
 		CloseHandle(hFile);
 		return TRUE;
 	}
 	CloseHandle(hFile);
-	printf("Multiboot header has been added successfully.");
-	//Create iso file.
-	system("c:/windows/sysnative/bash -c \"cp /mnt/c/users/0xCC/source/repos/OSKernel/Release/OSKernel.exe /mnt/c/users/0xCC/Desktop/os/boot/OSKernel.exe && grub-mkrescue -o /mnt/c/users/0xCC/Desktop/bootable0.iso /mnt/c/users/0xCC/Desktop/os/\"");
+	printf("Multiboot header has been added successfully.\n");
+
+	//Create iso file using grub-mkrescue in WSL.
+	std::string linux_outpath = argv[1];
+	linux_outpath = ReplaceAll(linux_outpath, "\\", "/");
+	linux_outpath = ReplaceAll(linux_outpath, "C:", "c");
+	
+	char wsl_command[0x200];
+	snprintf(wsl_command, sizeof(wsl_command), "c:/windows/sysnative/bash -c \"cp /mnt/%s /mnt/c/users/0xCC/Desktop/os/boot/OSKernel.exe && grub-mkrescue -o /mnt/c/users/0xCC/Desktop/bootable0.iso /mnt/c/users/0xCC/Desktop/os/\"", linux_outpath.c_str());
+	system(wsl_command);
+
 	return FALSE;
 }
